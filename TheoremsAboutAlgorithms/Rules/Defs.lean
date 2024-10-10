bae5ee4f23939e865f98b8d3a3d9f914efbcf8f3
Dennis Renz <dennis@r-renz.de>
2024-10-10 22:41:46 +0200

    WIP: Start `Rule` modeling 🎱

---
+++ TheoremsAboutAlgorithms/Rules/Defs.lean
@@ -0,0 +1,5 @@
+import Mathlib.Init.Data.Nat.Notation
+import Mathlib.Data.Finset.Basic
+
+abbrev Tag := Fin
+abbrev Tags (n : ℕ) := Finset (Tag n)
---
+++ TheoremsAboutAlgorithms/Rules/Instance.lean
@@ -0,0 +1,4 @@
+import TheoremsAboutAlgorithms.Rules.Defs
+
+structure Instance (n : ℕ) where
+  (tags : Tags n)
